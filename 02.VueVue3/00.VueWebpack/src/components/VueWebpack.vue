d2dbf53411d0ad170e6ff70c79b4a4ea1962b494
galadriel91 <g_ala@Naver.com>
2022-06-13 11:20:27 +0900

    Add: Components - VueWebpack

---
+++ 02.VueVue3/00.VueWebpack/src/components/VueWebpack.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <img :src="require('Images/webpack.png')" alt="웹팩 로고">
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        const title = ref('Vue Webpack Setting')
+        
+        return { title }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import 'Style/components/VueWebpack';
+</style>
