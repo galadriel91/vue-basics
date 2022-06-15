154c3f0c237cd05946fc187737913011d1613bb4
galadriel91 <g_ala@Naver.com>
2022-06-15 12:02:59 +0900

    Add: Components - TodoContainer

--- 03.VueTodo/App.vue
+++ 03.VueTodo/App.vue
@@ -2,16 +2,19 @@
     <div class="container">
         <TodoHeader/>
         <TodoInput/>
+        <TodoContainer/>
     </div>
 </template>
 
 <script>
 import TodoHeader from 'Components/TodoHeader.vue';
 import TodoInput from 'Components/TodoInput.vue';
+import TodoContainer from 'Components/TodoContainer.vue';
 export default {
     components:{
         TodoHeader,
-        TodoInput
+        TodoInput,
+        TodoContainer
     }
 }
 </script>
