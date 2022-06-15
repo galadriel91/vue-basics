12e9ae62307c97976d3e9985bd0943ae91f0bab7
galadriel91 <g_ala@Naver.com>
2022-06-15 11:34:40 +0900

    Add: Components - TodoInput

--- 03.VueTodo/App.vue
+++ 03.VueTodo/App.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container">
         <TodoHeader/>
+        <TodoInput/>
     </div>
 </template>
 
 <script>
 import TodoHeader from 'Components/TodoHeader.vue';
+import TodoInput from 'Components/TodoInput.vue';
 export default {
     components:{
-        TodoHeader
+        TodoHeader,
+        TodoInput
     }
 }
 </script>
---
+++ 03.VueTodo/src/components/TodoInput.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="onSubmitForm">
+        <input type="text" placeholder="오늘 해야할 일을 입력해 주세요" v-model="value" ref="input">
+        <button class="xi-arrow-right" type="submit"></button>
+    </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            value:'',
+        }
+    },
+    methods:{
+        // onSubmitForm(){
+        //     if(this.value.length){
+        //         this.$store.commit('ADD_TODO' , {
+        //             id:Date.now(),
+        //             content:this.value,
+        //             isCheck:false
+        //         })
+        //         this.value = ''
+        //         this.$refs.input.focus()
+        //     }else{
+        //         alert('다시 한번 확인 해 주세요')
+        //         this.$refs.input.focus()
+        //     }
+        // }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import 'Style/components/TodoInput';
+</style>
