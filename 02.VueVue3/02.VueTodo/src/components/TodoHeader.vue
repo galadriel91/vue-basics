311fafbe2c0acc74d82545628e28e66442482796
galadriel91 <g_ala@Naver.com>
2022-06-15 15:09:14 +0900

    Add: Components - TodoHeader

---
+++ 02.VueVue3/02.VueTodo/src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <h1>Vue3 Todo Basics</h1>
+    </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+    @import 'Style/components/TodoHeader';
+</style>
