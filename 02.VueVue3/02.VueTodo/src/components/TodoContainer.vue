1591dc96e9cead6c7752f85751e77509462e2509
galadriel91 <g_ala@Naver.com>
2022-06-16 10:55:29 +0900

    Add: Define GET_TODO in Vuex and connect to TodoContainer

--- 02.VueVue3/02.VueTodo/src/components/TodoContainer.vue
+++ 02.VueVue3/02.VueTodo/src/components/TodoContainer.vue
@@ -5,8 +5,24 @@
 </template>
 
 <script>
+    import { computed } from 'vue'
+    import { useStore } from 'vuex'
     export default {
         
+        setup(){
+            const store = useStore()
+            const getItem = () => {
+                store.commit('GET_TODO')
+            }
+            getItem()
+            const TODOS = computed(()=>{
+                return store.state.todos
+            })
+
+            return{
+                TODOS
+            }
+        }
     }
 </script>
 
--- 02.VueVue3/02.VueTodo/src/store/mutations.js
+++ 02.VueVue3/02.VueTodo/src/store/mutations.js
@@ -2,5 +2,8 @@ export default{
     ADD_TODO(state , payload){
         state.todos.push(payload)
         localStorage.setItem('todos' , JSON.stringify(state.todos))
+    },
+    GET_TODO(state){
+        localStorage.todos ? state.todos = JSON.parse(localStorage.todos) :[]
     }
 }
