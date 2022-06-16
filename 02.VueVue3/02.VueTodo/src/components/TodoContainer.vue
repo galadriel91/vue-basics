8c792f0fc4723d4b91c92f885742f1f4a954f733
galadriel91 <g_ala@Naver.com>
2022-06-16 11:01:17 +0900

    Add: Components - TodoItem

--- 02.VueVue3/02.VueTodo/src/components/TodoContainer.vue
+++ 02.VueVue3/02.VueTodo/src/components/TodoContainer.vue
@@ -1,14 +1,17 @@
 <template>
     <ul>
-
+        <TodoItem v-for="todo in TODOS" :key="todo.id" :item="todo"/>
     </ul>
 </template>
 
 <script>
+    import TodoItem from 'Components/TodoItem.vue';
     import { computed } from 'vue'
     import { useStore } from 'vuex'
     export default {
-        
+        components:{
+            TodoItem
+        },
         setup(){
             const store = useStore()
             const getItem = () => {
