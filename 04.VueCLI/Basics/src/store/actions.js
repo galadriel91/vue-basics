a214aef51c3370de538856cb3c936b3bdd4cec28
galadriel91 <g_ala@Naver.com>
2022-06-16 21:51:12 +0900

    Feature: Define loading function when moving pages

--- 04.VueCLI/Basics/src/store/actions.js
+++ 04.VueCLI/Basics/src/store/actions.js
@@ -3,6 +3,7 @@ import { fetchListItem } from '@/api'
 export default{
     async FETCH_LIST({commit} , payload){
         const { data } = await fetchListItem(payload);
+        commit('ON_LOADING')
         commit('FETCH_LIST_ITEM' , data)
         return data
     }
--- 04.VueCLI/Basics/src/store/mutations.js
+++ 04.VueCLI/Basics/src/store/mutations.js
@@ -1,5 +1,11 @@
 export default{
     FETCH_LIST_ITEM(state , payload){
         state.list = payload
+    },
+    ON_LOADING(state){
+        state.isLoading = true
+    },
+    OFF_LOADING(state){
+        state.isLoading = false
     }
 }
