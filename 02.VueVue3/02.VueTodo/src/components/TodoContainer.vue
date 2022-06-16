5ec3549a863b5360d68a79957d18cabe84d68e43
galadriel91 <g_ala@Naver.com>
2022-06-16 10:46:36 +0900

    Add: Components - TodoContainer

---
+++ 02.VueVue3/02.VueTodo/src/components/TodoContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+
+    </ul>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import 'Style/components/TodoContainer';
+</style>
