fabbc67344a6e96bd4970bd638d7575703f3d5da
galadriel91 <g_ala@Naver.com>
2022-06-18 00:45:36 +0900

    Feature: Using API to get data when accessing a page Add Ask and Jobs

--- 04.VueCLI/Advance/src/pages/ListPage.vue
+++ 04.VueCLI/Advance/src/pages/ListPage.vue
@@ -6,20 +6,10 @@
 
 <script>
     import ListItem from '@/components/list/ListItem.vue';
-    import { useStore } from 'vuex'
-    import { useRoute } from 'vue-router'
     export default {
         components:{
             ListItem
         },
-        setup(){
-            const store = useStore()
-            const route = useRoute()
-            const getList = () => {
-                store.dispatch('FETCH_LIST' , route.name)
-            }
-            getList()
-        }
     }
 </script>
 
--- 04.VueCLI/Advance/src/routes/index.js
+++ 04.VueCLI/Advance/src/routes/index.js
@@ -1,4 +1,13 @@
 import { createWebHashHistory , createRouter } from 'vue-router'
+import { store } from '@/store/index'
+
+const getPage = () => (to , from , next) => {
+    store.dispatch('FETCH_LIST' , to.name)
+    .then(()=>{
+        next()
+    })
+}
+
 
 export const router = new createRouter({
     history:createWebHashHistory(),
@@ -10,7 +19,20 @@ export const router = new createRouter({
         {
             path:'/news',
             name:'news',
-            component:()=> import('@/pages/ListPage')
+            component:()=> import('@/pages/ListPage'),
+            beforeEnter:getPage()
+        },
+        {
+            path:'/ask',
+            name:'ask',
+            component:()=> import('@/pages/ListPage'),
+            beforeEnter:getPage()
+        },
+        {
+            path:'/jobs',
+            name:'jobs',
+            component:()=> import('@/pages/ListPage'),
+            beforeEnter:getPage()
         }
     ]
 })
