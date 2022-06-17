fe8add273f5a7919ba54d6497abef03b7f878b0c
galadriel91 <g_ala@Naver.com>
2022-06-17 14:38:32 +0900

    Feature: Connect to Vuex and VueRouter to Vue

---
+++ 04.VueCLI/Advance/src/routes/index.js
@@ -0,0 +1,8 @@
+import { createWebHashHistory , createRouter } from 'vue-router'
+
+export const router = new createRouter({
+    history:createWebHashHistory(),
+    routes:[
+        
+    ]
+})
---
+++ 04.VueCLI/Advance/src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+
+export const store = createStore({
+    state:{
+
+    },
+    mutations,
+    actions
+})
