0d67ddfb01e9736f38a27d4591db40ae77b44061
galadriel91 <g_ala@Naver.com>
2022-06-18 13:37:26 +0900

    Add: Components - Common - NuxtLoading

---
+++ 06.VueNuxt/Basics/components/common/NuxtLoading.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="loadingWrap" v-if="loading">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        loading:{
+            type:Boolean,
+            required:true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/scss/components/common/NuxtLoading';
+</style>
--- 06.VueNuxt/Basics/layouts/default.vue
+++ 06.VueNuxt/Basics/layouts/default.vue
@@ -2,16 +2,24 @@
     <div>
         <NuxtHeader/>
         <NuxtFooter/>
+        <NuxtLoading :loading="LOADING"/>
     </div>
 </template>
 
 <script>
     import NuxtHeader from '@/components/common/NuxtHeader.vue';
     import NuxtFooter from '@/components/common/NuxtFooter.vue';
+    import NuxtLoading from '@/components/common/NuxtLoading.vue';
     export default {
         components:{
             NuxtHeader,
-            NuxtFooter
+            NuxtFooter,
+            NuxtLoading
+        },
+        computed:{
+            LOADING(){
+                return this.$store.state.isLoading
+            }
         }
     }
 </script>
