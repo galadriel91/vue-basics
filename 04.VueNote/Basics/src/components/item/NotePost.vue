d55da9c0b326ba5a4be050b4c34e05286148dbb6
galadriel91 <g_ala@Naver.com>
2022-06-23 20:58:07 +0900

    Update: NotePost Basics Style

--- 04.VueNote/Basics/src/components/item/NotePost.vue
+++ 04.VueNote/Basics/src/components/item/NotePost.vue
@@ -1,27 +1,24 @@
 <template>
     <div class="inputWrap">
-        <form @submit.prevent="onSubmitForm">
-            <div class="formWrap">
-                <div>
-                    <label for="title">제목</label>
-                    <input id="title" type="text" :placeholder="item.title" v-model="title" ref="titleInput">
-                </div>
-                <div>
-                    <label for="content">내용</label>
-                    <textarea id="content" :placeholder="item.content" v-model="content" ></textarea>
-                </div>
+        <div class="formWrap">
+            <div class="titleWrap">
+                <h2>{{item.title}}</h2>
+                <!-- <label for="title">제목</label> -->
+                <!-- <input id="title" type="text" :placeholder="item.title" v-model="title" ref="titleInput"> -->
             </div>
-            <div class="infoWrap" :class="{noUpdate : !item.update}">
-                <div class="dateWrap" v-if="item.update">
-                    <span>최근 수정일:</span>
-                    <span>{{DATE}}</span>
-                </div>
-                <div class="buttonWrap">
-                    <button @click="onClickMain">취소</button>
-                    <button type="submit">수정</button>
-                </div>
+            <div class="contentWrap">
+                <p>{{item.content}}</p>
+                <!-- <label for="content">내용</label> -->
+                <!-- <textarea id="content" :placeholder="item.content" v-model="content" ></textarea> -->
             </div>
-        </form>
+        </div>
+        <div class="postRight">
+            <button>이전</button>
+            <div class="buttonWrap">
+                <button @click="onClickMain">수정</button>
+                <button type="submit">삭제</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -73,12 +70,9 @@ export default {
             return result
         }
     },
-    mounted(){
-        this.$refs.titleInput.focus()
-    }
 }
 </script>
 
 <style lang="scss" scoped>
-    @import 'Style/components/form/NoteEdit';
+    @import 'Style/components/item/NotePost';
 </style>
