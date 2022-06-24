2a9e70624ae3f7311178b8e98327eda575660496
galadriel91 <g_ala@Naver.com>
2022-06-24 11:05:07 +0900

    <Vue Vite Initial Commit>

---
+++ 07.VueVite/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        App
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    div{
+        color:red
+    }
+</style>
