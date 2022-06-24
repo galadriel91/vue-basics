5321945f054ca30208362a7d42a11c4dfa406092
galadriel91 <g_ala@Naver.com>
2022-06-24 09:42:11 +0900

    Add: Add Pages and define routs path

--- 04.VueNote/Advance/src/routes/index.js
+++ 04.VueNote/Advance/src/routes/index.js
@@ -3,6 +3,25 @@ import { createRouter , createWebHashHistory } from 'vue-router'
 export const router = createRouter({
     history:createWebHashHistory(),
     routes:[
-        
+        {
+            path:'/',
+            name:'main',
+            component:()=>import('Pages/MainPage')
+        },
+        {
+            path:'/create',
+            name:'create',
+            component:()=>import('Pages/CreatePage')
+        },
+        {
+            path:'/edit/:id',
+            name:'edit',
+            component:()=>import('Pages/EditPage')
+        },
+        {
+            path:'/item/:id',
+            name:'post',
+            component:()=>import('Pages/ItemPage')
+        }
     ]
 })
