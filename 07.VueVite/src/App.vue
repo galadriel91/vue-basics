fcda1dbd9dd076e6c1fddce18b23a05169d99c26
galadriel91 <g_ala@Naver.com>
2022-06-26 15:17:15 +0900

    Feature: Define Get_Note in Vuex

--- 07.VueVite/src/App.vue
+++ 07.VueVite/src/App.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="container">
+    <div>
         <NoteHeader/>
-        <RouterView/>
+        <div class="container">
+            <RouterView/>
+        </div>
     </div>
 </template>
 
 <script>
+    import { onMounted } from 'vue'
+    import { useStore } from 'vuex'
     import NoteHeader from '@/components/common/NoteHeader.vue';
     export default {
         components:{
             NoteHeader
+        },
+        setup(){
+            const store = useStore()
+            onMounted(()=>{
+                store.commit('GET_NOTE')
+            })
         }
     }
 </script>
+
 
 <style lang="scss">
     @import '@/assets/scss/index';
