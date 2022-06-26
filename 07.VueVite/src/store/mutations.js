a55be638092dae7eb0898d46220d59c058d31262
galadriel91 <g_ala@Naver.com>
2022-06-26 14:57:51 +0900

    Feature: Define Add_Note in Vuex

--- 07.VueVite/src/store/mutations.js
+++ 07.VueVite/src/store/mutations.js
@@ -1,3 +1,9 @@
 export default{
-    
+    ADD_NOTE(state , payload){
+        state.notes.unshift(payload)
+        localStorage.setItem('notes' , JSON.stringify(state.notes))
+    },
+    GET_NOTE(state){
+        localStorage.notes ? state.notes = JSON.parse(localStorage.notes) : []
+    },
 }
