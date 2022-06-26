2d4de6d41847339b577451bbf57a595fcbce1911
galadriel91 <g_ala@Naver.com>
2022-06-26 18:55:25 +0900

    Feature: Apply afterEach in Routes

--- 07.VueVite/src/routes/index.js
+++ 07.VueVite/src/routes/index.js
@@ -1,7 +1,14 @@
 import { createWebHashHistory , createRouter } from 'vue-router'
+import { nextTick } from 'vue'
 import path from './path'
 
 export const router = createRouter({
     history:createWebHashHistory(),
     routes:path
 })
+
+router.afterEach((to) => {
+    nextTick(() => {
+        document.title = `${to.meta.title}`
+    });
+});
