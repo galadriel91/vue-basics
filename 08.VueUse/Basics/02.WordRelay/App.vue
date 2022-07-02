0f5f7801ae9e8e36ba2f84e429f260aad91c96ad
galadriel91 <g_ala@Naver.com>
2022-07-02 21:48:40 +0900

    Add: StyleSheets

--- 08.VueUse/Basics/02.WordRelay/App.vue
+++ 08.VueUse/Basics/02.WordRelay/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         App
     </div>
 </template>
@@ -10,6 +10,6 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+    @import 'Style/index';
 </style>
