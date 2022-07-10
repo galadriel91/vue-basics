b9b66607f8642ec81b1a54bc4b0489a35dc9ac82
galadriel91 <g_ala@Naver.com>
2022-07-10 09:30:24 +0900

    Add: StyleSheets

--- 08.VueUse/Advance/07.TicTacToe/App.vue
+++ 08.VueUse/Advance/07.TicTacToe/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     App
   </div>
 </template>
@@ -14,6 +14,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+  @import 'Style/index';
 </style>
