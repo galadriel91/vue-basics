4ae95fd280220c435b34d18e3ec5a0e1687c5235
GalaDriel91 <g_ala@naver.com>
2022-07-11 14:35:04 +0900

    <VueApi Basics Initial Commit>

---
+++ 09.VueApi/Basics/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <TodoContainer />
+    </div>
+</template>
+
+<script>
+import TodoContainer from '@/components/TodoContainer.vue';
+export default {
+    components: {
+        TodoContainer,
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
