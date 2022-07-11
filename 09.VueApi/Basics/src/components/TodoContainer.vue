684fe202b5a6cc37c60c477d888a3680095124ef
GalaDriel91 <g_ala@naver.com>
2022-07-11 14:36:52 +0900

    Add: Components - TodoContainer

---
+++ 09.VueApi/Basics/src/components/TodoContainer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>TodoContainer</div>
+</template>
+
+<script>
+import { provide, readonly, ref } from 'vue';
+export default {
+    setup() {
+        const todos = ref([]);
+        provide('todos', readonly(todos));
+
+        const addTodo = (todo, date) => {};
+        const removeTodo = id => {};
+        const completeTodo = id => {};
+
+        provide('addTodo', addTodo);
+        provide('removeTodo', removeTodo);
+        provide('completeTodo', completeTodo);
+
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
