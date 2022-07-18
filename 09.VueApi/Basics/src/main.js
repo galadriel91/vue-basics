9b93c6c8bb5ff7bb4c910fb4c08490136488cf88
GalaDriel91 <g_ala@naver.com>
2022-07-18 23:19:27 +0900

    Feature: Connect pinia to vue

--- 09.VueApi/Basics/src/main.js
+++ 09.VueApi/Basics/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import { router } from '@/routes';
+import { createPinia } from 'pinia';
+const pinia = createPinia();
 import App from './App.vue';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
---
+++ 09.VueApi/Basics/src/store/index.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+export const useStore = defineStore({
+    state: () => ({
+        test: 1,
+    }),
+    actions: {},
+});
