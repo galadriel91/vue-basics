d42cc8938f3dab578556cb4599605fe70f04bdef
GalaDriel91 <g_ala@naver.com>
2022-07-19 22:27:05 +0900

    Add: Components - common - NoteFooter

--- 09.VueApi/Basics/src/App.vue
+++ 09.VueApi/Basics/src/App.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <NoteHeader />
+        <NoteFooter />
     </div>
 </template>
 
 <script>
 import NoteHeader from '@/components/common/NoteHeader.vue';
+import NoteFooter from '@/components/common/NoteFooter.vue';
 export default {
     components: {
         NoteHeader,
+        NoteFooter,
     },
     setup() {
         return {};
---
+++ 09.VueApi/Basics/src/components/common/NoteFooter.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        <div class="footerWrap">
+            <h2>© {{ YEAR }} LEE MOON SEOB</h2>
+            <a
+                href="https://github.com/galadriel91"
+                target="_blank"
+                class="xi-github"
+            ></a>
+        </div>
+    </footer>
+</template>
+
+<script>
+import { computed } from 'vue';
+export default {
+    setup() {
+        const YEAR = computed(() => {
+            return new Date().getFullYear();
+        });
+        return { YEAR };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/common/NoteFooter';
+</style>
