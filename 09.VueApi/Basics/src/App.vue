43370f9a2191360b080c89e3928b959c8f0ad42a
GalaDriel91 <g_ala@naver.com>
2022-07-19 22:38:32 +0900

    Add: Components - common - NoteLoading

--- 09.VueApi/Basics/src/App.vue
+++ 09.VueApi/Basics/src/App.vue
@@ -2,16 +2,19 @@
     <div>
         <NoteHeader />
         <NoteFooter />
+        <NoteLoading />
     </div>
 </template>
 
 <script>
 import NoteHeader from '@/components/common/NoteHeader.vue';
 import NoteFooter from '@/components/common/NoteFooter.vue';
+import NoteLoading from '@/components/common/NoteLoading.vue';
 export default {
     components: {
         NoteHeader,
         NoteFooter,
+        NoteLoading,
     },
     setup() {
         return {};
---
+++ 09.VueApi/Basics/src/components/common/NoteLoading.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="loadingWrap" v-if="LOADING">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+    setup() {
+        const LOADING = computed(() => {
+            return false;
+        });
+        return {
+            LOADING,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/common/NoteLoading';
+</style>
