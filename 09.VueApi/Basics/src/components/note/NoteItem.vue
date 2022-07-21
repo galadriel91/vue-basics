6273dc314d5cd95664737beddadaddff607014d1
GalaDriel91 <g_ala@naver.com>
2022-07-21 15:07:58 +0900

    Add: Components - note - NoteItem

---
+++ 09.VueApi/Basics/src/components/note/NoteItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <li class="noteItem">
+        <div class="contentWrap">
+            <h3 class="contentWrapTitle">{{ item.title }}</h3>
+            <div class="contentWrapContent">
+                <p>
+                    {{ item.content }}
+                </p>
+            </div>
+        </div>
+        <div class="contentInfoWrap">
+            <div class="dateWrap">
+                <!-- <span>{{ DATE }}</span> -->
+                <!-- <span v-if="item.update">최근 수정</span> -->
+            </div>
+            <div class="btnWrap">
+                <button class="xi-pen-o"></button>
+                <button class="xi-minus-square-o"></button>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: false,
+        },
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/note/NoteItem';
+</style>
--- 09.VueApi/Basics/src/pages/MainPage.vue
+++ 09.VueApi/Basics/src/pages/MainPage.vue
@@ -1,17 +1,31 @@
 <template>
-    <div>Main</div>
+    <div>
+        <ul v-if="notes.length">
+            <NoteItem v-for="note in notes" :key="note.id" :item="note" />
+        </ul>
+    </div>
 </template>
 
 <script>
+import NoteItem from '@/components/note/NoteItem.vue';
 import { fetchNoteItem } from '@/api';
+import { ref } from 'vue';
 export default {
+    components: {
+        NoteItem,
+    },
     setup() {
+        const notes = ref([]);
         const fetchItem = async () => {
             const { data } = await fetchNoteItem();
             console.log(data);
+            notes.value = data;
+            return data;
         };
         fetchItem();
-        return {};
+        return {
+            notes,
+        };
     },
 };
 </script>
