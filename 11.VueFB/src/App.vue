a9f501a64753a7d0bc6b8a5f09cfec938d1c0688
GalaDriel91 <g_ala@naver.com>
2022-07-23 14:57:15 +0900

    Add: Components - TodoContainer

--- 11.VueFB/src/App.vue
+++ 11.VueFB/src/App.vue
@@ -2,16 +2,19 @@
     <div class="container">
         <TodoHeader />
         <TodoInput />
+        <TodoContainer />
     </div>
 </template>
 
 <script>
 import TodoHeader from '@/components/TodoHeader.vue';
 import TodoInput from '@/components/TodoInput.vue';
+import TodoContainer from '@/components/TodoContainer.vue';
 export default {
     components: {
         TodoHeader,
         TodoInput,
+        TodoContainer,
     },
 };
 </script>
---
+++ 11.VueFB/src/components/TodoContainer.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+        <!-- <TodoItem v-for="todo in TODOS" :key="todo.id" :item="todo" /> -->
+    </ul>
+</template>
+
+<script>
+// import TodoItem from 'Components/TodoItem.vue';
+// import { computed } from 'vue';
+// import { useStore } from 'vuex';
+// export default {
+//     components: {
+//         TodoItem,
+//     },
+//     setup() {
+//         const store = useStore();
+//         const getItem = () => {
+//             store.commit('GET_TODO');
+//         };
+//         getItem();
+//         const TODOS = computed(() => {
+//             return store.state.todos;
+//         });
+
+//         return {
+//             TODOS,
+//         };
+//     },
+// };
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/TodoContainer';
+</style>
