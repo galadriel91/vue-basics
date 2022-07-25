b2e706a34b6d59ec3379b3811c7f94ee42f18add
GalaDriel91 <g_ala@naver.com>
2022-07-25 10:33:46 +0900

    Feature: When get, add and remove todoItem, Apply loadingSpiner

--- 11.VueFB/src/App.vue
+++ 11.VueFB/src/App.vue
@@ -12,12 +12,24 @@ import TodoHeader from '@/components/TodoHeader.vue';
 import TodoInput from '@/components/TodoInput.vue';
 import TodoContainer from '@/components/TodoContainer.vue';
 import TodoLoading from '@/components/TodoLoading.vue';
+import { useStore } from '@/store';
+import { watch } from 'vue';
+import { storeToRefs } from 'pinia';
 export default {
     components: {
         TodoHeader,
         TodoInput,
         TodoContainer,
         TodoLoading,
+    },
+    setup() {
+        const store = useStore();
+        const { todos } = storeToRefs(store);
+        const { offLoading } = store;
+        watch(todos, () => {
+            console.log(1);
+            offLoading();
+        });
     },
 };
 </script>
--- 11.VueFB/src/store/index.js
+++ 11.VueFB/src/store/index.js
@@ -7,19 +7,28 @@ export const useStore = defineStore('store', {
         loading: false,
     }),
     actions: {
+        onLoading() {
+            this.loading = true;
+        },
+        offLoading() {
+            this.loading = false;
+        },
         async addTodoItem(todo) {
+            this.onLoading();
             await dbService.collection('todos').add(todo);
         },
         async checkTodoItem({ id, isCheck }) {
             await dbService.doc(`/todos/${id}`).update({ isCheck });
         },
         async removeTodoItem(id) {
+            this.onLoading();
             await dbService.doc(`/todos/${id}`).delete();
         },
         async updateTodoItem({ id, content }) {
             await dbService.doc(`/todos/${id}`).update({ content });
         },
-        getTodoItem() {
+        async getTodoItem() {
+            this.onLoading();
             dbService.collection('todos').onSnapshot(snap => {
                 const todos = snap.docs.map(todo => ({
                     ...todo.data(),
