c1681514da5179c43330a2b1e2d5721a9ef2ed29
GalaDriel91 <g_ala@naver.com>
2022-07-26 16:18:26 +0900

    Add: Components - note - NoteItem

---
+++ 09.VueApi/src/components/note/NoteItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <li class="noteItem">
+        <div class="contentWrap">
+            <h3 class="contentWrapTitle">{{ item.title }}</h3>
+            <div class="contentWrapContent">
+                <p>
+                    {{ item.content }}
+                </p>
+            </div>
+        </div>
+        <div class="contentInfoWrap">
+            <div class="dateWrap">
+                <!-- <span>{{ DATE }}</span> -->
+                <!-- <span v-if="item.update">최근 수정</span> -->
+            </div>
+            <div class="btnWrap">
+                <button class="xi-pen-o" @click="onClickEditPage"></button>
+                <button
+                    class="xi-minus-square-o"
+                    @click="onClickRemoveItem"
+                ></button>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+import { useStore } from '@/store';
+import { useRouter } from 'vue-router';
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+    setup(props) {
+        const store = useStore();
+        const router = useRouter();
+        // const { onRemoveItem } = store;
+        const onClickRemoveItem = () => {
+            // onRemoveItem(props.item.id);
+        };
+        const onClickEditPage = () => {
+            // router.push(`/edit/${props.item.id}`);
+        };
+        return {
+            onClickRemoveItem,
+            onClickEditPage,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/note/NoteItem';
+</style>
--- 09.VueApi/src/pages/MainPage.vue
+++ 09.VueApi/src/pages/MainPage.vue
@@ -1,22 +1,30 @@
 <template>
     <div>
-        <ul>
-            <!-- <NoteItem v-for="note in notes" :key="note.id" :item="note" /> -->
+        <ul v-if="notes.length">
+            <NoteItem v-for="note in notes" :key="note.id" :item="note" />
         </ul>
     </div>
 </template>
 
 <script>
+import NoteItem from '@/components/note/NoteItem.vue';
 import { useStore } from '@/store';
+import { storeToRefs } from 'pinia';
 import { onMounted } from 'vue';
 export default {
+    components: {
+        NoteItem,
+    },
     setup() {
         const store = useStore();
+        const { notes } = storeToRefs(store);
         const { getNote } = store;
         onMounted(() => {
             getNote();
         });
-        return {};
+        return {
+            notes,
+        };
     },
 };
 </script>
