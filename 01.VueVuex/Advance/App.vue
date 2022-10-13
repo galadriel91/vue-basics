bf091e3d937f0e47eb9bc19a5cd2038ec69dd609
GalaDriel <g_ala@naver.com>
2022-10-13 11:15:37 +0900

    <VueVuex Advance Initial Commit>

---
+++ 01.VueVuex/Advance/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <!-- <VueVuex/> -->
+        App
+    </div>
+</template>
+
+<script>
+// import VueVuex from 'Components/VueVuex.vue';
+export default {
+    components:{
+        // VueVuex
+    }
+}
+</script>
+
+<style lang="scss">
+    // @import 'Style/index';
+</style>
---
+++ 01.VueVuex/Advance/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+// import { store } from 'Store/index'
+import App from './App'
+
+// createApp(App).use(store).mount('#root')
+createApp(App).mount('#root')
