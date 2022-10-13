f9bb6def6d50c6e7bbcb4d88a2a7e36aa5cd2f60
GalaDriel <g_ala@naver.com>
2022-10-13 10:56:09 +0900

    <VueVuex Basics Initial Commit>

---
+++ 01.VueVuex/Basics/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        Vuex
+    </div>
+</template>
+
+<script>
+    // import VueVuex from 'Components/VueVuex.vue';
+    export default {
+        components:{
+            // VueVuex
+        }
+    }
+</script>
+
+<style lang="scss">
+    // @import 'Style/index';
+</style>
