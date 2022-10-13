376477236f56896ad72b8dbd300ba14c1d9d38dd
GalaDriel <g_ala@naver.com>
2022-10-13 11:26:30 +0900

    Update: Connect Vuex to vue

--- 01.VueVuex/Advance/main.js
+++ 01.VueVuex/Advance/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-// import { store } from 'Store/index'
+import { store } from 'Store/index'
 import App from './App'
 
-// createApp(App).use(store).mount('#root')
-createApp(App).mount('#root')
+createApp(App).use(store).mount('#root')
