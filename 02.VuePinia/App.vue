6bddd1cccdc4ec3c6da47f3c9ae419615077c2e8
GalaDriel <g_ala@naver.com>
2022-10-14 11:18:08 +0900

    <Vue Pinia Initial Commit>

---
+++ 02.VuePinia/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <!-- <VuePinia/> -->
+        App
+    </div>
+</template>
+
+<script>
+// import VuePinia from 'Components/VuePinia.vue';
+export default {
+    components:{
+        // VuePinia
+    },
+}
+</script>
+
+<style lang="scss">
+    // @import 'Style/index';
+</style>
