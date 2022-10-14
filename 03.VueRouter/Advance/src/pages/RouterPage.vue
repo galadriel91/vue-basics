46c1974b6cdfaca40192ed54b66472e5b0adc7c6
GalaDriel <g_ala@naver.com>
2022-10-14 13:34:03 +0900

    Add: Pages - Main & Router & Vuex

---
+++ 03.VueRouter/Advance/src/pages/RouterPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Vue & VueRouter Setting</h1>
+        <img :src="require('Images/router.png')" alt="뷰 라우터 로고">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import 'Style/pages/MainPage';
+</style>
---
+++ 03.VueRouter/Advance/src/pages/VuexPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Vue Vuex Setting</h1>
+        <img :src="require('Images/vuex.png')" alt="Vuex 로고">
+        <p>
+            {{num}}
+        </p>
+        <div>
+            <button @click="onClickPlusNum">Plus</button>
+            <button @click="onClickMinusNum">Minus</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { usePinia } from 'Store/index'
+import { storeToRefs } from 'pinia';
+export default {
+    setup(){
+        const pinia = usePinia();
+        const {num} = storeToRefs(pinia)
+        const {ADD_NUM , MINUS_NUM} = pinia
+        const onClickPlusNum = () => {
+            ADD_NUM()
+        }
+        const onClickMinusNum = () => {
+            MINUS_NUM()
+        }
+        return{ num , onClickPlusNum , onClickMinusNum}
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import 'Style/pages/VuexPage';
+</style>
