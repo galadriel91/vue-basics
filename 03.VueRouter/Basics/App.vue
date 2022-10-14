9d86aac009a1a18e3df7e729d27bd6fdc2bbb05c
GalaDriel <g_ala@naver.com>
2022-10-14 11:35:07 +0900

    <VueRouter Basics Initial Commit>

---
+++ 03.VueRouter/Basics/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        App
+        <!-- <RouterNav/>
+        <div class="container">
+            <RouterView/>
+        </div>         -->
+    </div>
+</template>
+
+<script>
+// import RouterNav from 'Components/RouterNav.vue';
+export default {
+    components:{
+        // RouterNav
+    }
+}
+</script>
+
+<style lang="scss">
+    // @import 'Style/index';
+</style>
---
+++ 03.VueRouter/Basics/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+// import { router } from 'Router/index'
+// import { store } from 'Store/index'
+
+new Vue({
+    render: h => h(App),
+    // router,
+    // store
+}).$mount('#root')
