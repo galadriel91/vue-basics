18585e7760bea21539fdf35257c762eba64f67fc
GalaDriel <g_ala@naver.com>
2022-10-15 11:59:14 +0900

    Update: Define types in Api

--- 05.VueApi/src/api/index.ts
+++ 05.VueApi/src/api/index.ts
@@ -1,18 +1,20 @@
-import axios from 'axios';
+import type { ListItems, PostsItem, UserItems } from '@/store/types';
+
+import axios, { type AxiosResponse } from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://api.hnpwa.com/v0/',
 });
 
-const fetchListItem = (pageName: string) => {
+const fetchListItem = (pageName: string): Promise<AxiosResponse<ListItems>> => {
     return instance.get(`${pageName}/1.json`);
 };
 
-const fetchUserItem = (userId: string) => {
+const fetchUserItem = (userId: string): Promise<AxiosResponse<UserItems>> => {
     return instance.get(`user/${userId}.json`);
 };
 
-const fetchPostItem = (postId: string) => {
+const fetchPostItem = (postId: string): Promise<AxiosResponse<PostsItem>> => {
     return instance.get(`item/${postId}.json`);
 };
 
