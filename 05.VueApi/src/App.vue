8e5999e39208a69d3831f8629b1c635ce833d386
GalaDriel <g_ala@naver.com>
2022-10-15 10:40:07 +0900

    Add: Components - Common - CommonLoading

--- 05.VueApi/src/App.vue
+++ 05.VueApi/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <CommonFooter />
+        <CommonLoading />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import CommonFooter from './components/common/CommonFooter.vue';
+import CommonLoading from './components/common/CommonLoading.vue';
 
 export default defineComponent({
     components: {
         CommonFooter,
+        CommonLoading,
     },
     setup() {
         return {};
