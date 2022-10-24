0201d489d7f011400e1ac5cddb34278cc2a51d77
GalaDriel <g_ala@naver.com>
2022-10-24 10:21:47 +0900

    Update: Connect firebase to vue

--- 08.VueFB/src/App.vue
+++ 08.VueFB/src/App.vue
@@ -8,11 +8,13 @@
 </template>
 
 <script lang="ts">
+import { app } from './utils/fb';
 import { defineComponent } from 'vue';
 import TodoLoading from '@/components/TodoLoading.vue';
 import TodoHeader from './components/TodoHeader.vue';
 import TodoInput from '@/components/TodoInput.vue';
 import TodoContainer from './components/TodoContainer.vue';
+console.log(app);
 export default defineComponent({
     components: {
         TodoLoading,
