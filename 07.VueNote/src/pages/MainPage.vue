79f6847abe2d5c717130aea722a1c1957d36c404
GalaDriel <g_ala@naver.com>
2022-10-27 11:18:43 +0900

    Add: Components - Note - NoteItem

--- 07.VueNote/src/pages/MainPage.vue
+++ 07.VueNote/src/pages/MainPage.vue
@@ -1,33 +1,32 @@
 <template>
     <div>
-        MainPage
-        <!-- <ul>
+        <ul>
             <NoteItem v-for="note in notes" :item="note" :key="note.id" />
         </ul>
-        <NotePagination v-if="parseInt(totalItems) > 3" :keyword="''" /> -->
+        <!-- <NotePagination v-if="parseInt(totalItems) > 3" :keyword="''" /> -->
     </div>
 </template>
 
 <script lang="ts">
 // import NotePagination from '../components/note/NotePagination.vue';
-// import NoteItem from '../components/note/NoteItem.vue';
+import NoteItem from '../components/note/NoteItem.vue';
 // import { useLoading } from '@/composables/useLoading';
-// import { useItem } from '@/store/itemStore';
+import { useItem } from '@/store/itemStore';
 import { defineComponent } from 'vue';
-// import { storeToRefs } from 'pinia';
+import { storeToRefs } from 'pinia';
 
 export default defineComponent({
     components: {
-        // NoteItem,
+        NoteItem,
         // NotePagination,
     },
     setup() {
         // useLoading();
-        // const item = useItem();
-        // const { notes, totalItems } = storeToRefs(item);
+        const item = useItem();
+        const { notes } = storeToRefs(item);
 
         return {
-            // notes,
+            notes,
             // totalItems,
         };
     },
