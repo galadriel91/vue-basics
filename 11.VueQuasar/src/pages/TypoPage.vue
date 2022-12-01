2e2a674c79a0895b61b4537e332e38a62b295b83
GalaDriel <g_ala@naver.com>
2022-12-02 08:16:30 +0900

    Update: Pages - TypoPage

--- 11.VueQuasar/src/pages/TypoPage.vue
+++ 11.VueQuasar/src/pages/TypoPage.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="q-pa-xl">
-        <section class="q-my-md">
-            Headings
-            <q-separator/>
+    <q-page class="q-pa-xl">
+        <section>
+            <div class="text-h4">Headings</div>
+            <q-separator class="q-my-md"/>
             <p class="text-h1">Headline 1</p>
             <p class="text-h2">Headline 2</p>
             <p class="text-h3">Headline 3</p>
@@ -14,15 +14,15 @@
             <p class="text-body1">Body 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
             <p class="text-body2">Body 2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aliquid ad quas sunt voluptatum officia dolorum cumque, possimus nihil molestias sapiente necessitatibus dolor saepe inventore, soluta id accusantium voluptas beatae.</p>
         </section>
-        <q-separator/>
-        <section section class="q-my-md">
-            Font
-            <q-separator/>
+        <q-separator class="q-my-md"/>
+        <section>
+            <div class="text-h4">Font</div>
+            <q-separator class="q-my-md"/>
             <p class="text-weight-thin text-right">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             <p class="text-weight-regular text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             <p class="text-weight-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </section>
-    </div>
+    </q-page>
 </template>
 
 <script lang="ts">
