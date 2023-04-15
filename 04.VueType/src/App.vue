2c3ac05619f4e7a81356bf5ffbd4f08f7e738c77
GalaDriel <g_ala@naver.com>
2023-04-15 18:58:20 +0900

    Add: Components - TodoItem

--- 04.VueType/src/App.vue
+++ 04.VueType/src/App.vue
@@ -2,12 +2,16 @@
     <div class="container">
         <TodoHeader/>
         <TodoInput/>
+        <ul>
+            <TodoItem/>
+        </ul>
     </div>
 </template>
 
 <script setup lang="ts">
 import TodoHeader from '@/components/TodoHeader.vue';
 import TodoInput from '@/components/TodoInput.vue';
+import TodoItem from '@/components/TodoItem.vue';
 
 
 </script>
---
+++ 04.VueType/src/components/TodoItem.vue
@@ -0,0 +1,56 @@
+<template>
+    <li>
+        아이템
+        <!-- <div class="itemWrap">
+            <div class="checkWrap">
+                <button
+                    class="xi-checkbox-blank"
+                    v-if="!todo.isCheck"
+                    @click="onClickCheck"
+                ></button>
+                <button
+                    class="xi-check-square-o"
+                    v-else
+                    @click="onClickCheck"
+                ></button>
+            </div>
+            <div class="titleWrap">
+                <h4 :class="{ complete: todo.isCheck }" v-if="!isUpdate">
+                    {{ todo.title }}
+                </h4>
+                <form v-else @submit.prevent="onSubmitUpdate">
+                    <input
+                        type="text"
+                        :placeholder="todo.title"
+                        ref="update"
+                        v-model="value"
+                    />
+                    <button
+                        type="submit"
+                        class="xi-check-min"
+                        @mousedown="onSubmitUpdate"
+                    ></button>
+                </form>
+                <button
+                    class="xi-pen updateBtn"
+                    @click="onClickUpdate"
+                    v-if="!isUpdate"
+                ></button>
+            </div>
+            <div class="buttonWrap">
+                <button
+                    class="xi-minus-square-o"
+                    @click="onClickRemove"
+                ></button>
+            </div>
+        </div> -->
+    </li>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
