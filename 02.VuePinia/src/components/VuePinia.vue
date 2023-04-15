08630fbb81730dd017aa1aa6cbec91a30c38cb33
GalaDriel <g_ala@naver.com>
2023-04-15 11:48:47 +0900

    Update: Define Pinia & Components - VuePinia

--- 02.VuePinia/src/components/VuePinia.vue
+++ 02.VuePinia/src/components/VuePinia.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="number">{{ initNum }}</div>
+    <div class="number">{{ number }}</div>
     <div class="btnWrap">
         <button @click="onClickPlus">+</button>
         <button @click="onClickMinus">-</button>
@@ -7,15 +7,23 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { piniaStore } from '../store/piniaStore';
+import { storeToRefs } from 'pinia';
 
-const initNum = ref(0)
-const onClickPlus = () => { 
-    initNum.value++
+const store = piniaStore();
+const { number } = storeToRefs(store)
+// const { onClickMinus , onClickPlus} = store
+
+// 스토어 액션을 거치지 않고 바로 스테이트 값 변경이 가능 - Vuex와 다름
+const onClickPlus = () => {
+    number.value++
 }
+
 const onClickMinus = () => {
-    initNum.value--
+    number.value--
 }
+
+
 </script>
 
 <style lang="scss" scoped>
