907388822fcba6a771cc1a94beb41d559213ef03
GalaDriel <g_ala@naver.com>
2023-04-15 10:08:08 +0900

    Update: Setup basics vue setting

---
+++ 01.VueVuex/src/components/VueComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="number">{{ initNum }}</div>
+    <div class="btnWrap">
+        <button @click="onClickPlus">+</button>
+        <button @click="onClickMinus">-</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const initNum = ref(0)
+const onClickPlus = () => { 
+    initNum.value++
+}
+const onClickMinus = () => {
+    initNum.value--
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/VueComponent';
+</style>
