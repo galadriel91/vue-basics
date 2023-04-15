3730b78625a3c00453808aae459f4f0578d3c2b2
GalaDriel <g_ala@naver.com>
2023-04-15 09:47:36 +0900

    Update: Define Components - VueComponent

--- 00.VueStart/src/components/VueComponent.vue
+++ 00.VueStart/src/components/VueComponent.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        Vue3
+    <div class="number">{{ initNum }}</div>
+    <div class="btnWrap">
+        <button @click="onClickPlus">+</button>
+        <button @click="onClickMinus">-</button>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const initNum = ref(0)
+const onClickPlus = () => { 
+    initNum.value++
+}
+const onClickMinus = () => {
+    initNum.value--
+}
 </script>
 
 <style lang="scss" scoped>
-
+@import '@/assets/scss/components/VueComponent';
 </style>
