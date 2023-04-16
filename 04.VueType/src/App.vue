9419c1a161713ea7ffe4874d8cf9c2ab4a17b1fe
GalaDriel <g_ala@naver.com>
2023-04-16 11:49:22 +0900

    Update: Define localstorage logic

--- 04.VueType/src/App.vue
+++ 04.VueType/src/App.vue
@@ -18,27 +18,48 @@
 
     const todos = ref([] as TodoItems[]);
 
-    const onAddItem = (title : string) => {
+    const storage = {
+        save(item: TodoItems[]) {
+            localStorage.setItem('todos', JSON.stringify(item));
+        },
+        fetch(): TodoItems[] {
+            const item = localStorage.getItem('todos') || '[]';
+            const todoItem = JSON.parse(item);
+            return todoItem;
+        },
+    };
+
+    const onGetItem = () => {
+        todos.value = storage.fetch();
+    };
+    onGetItem();
+
+    const onAddItem = (title:string) => {
         const todo = {
             id:Date.now(),
             title:title,
             isCheck:false,
         }
         todos.value.unshift(todo)
+        storage.save(todos.value);
     }
 
     const onCheckItem = (value:number) => {
         const index = todos.value.findIndex(v => v.id === value);
         todos.value[index].isCheck = !todos.value[index].isCheck;
+        storage.save(todos.value);
     }
     const onRemoveItem = (value: number) => {
         const index = todos.value.findIndex(v => v.id === value);
         todos.value.splice(index, 1);
+        storage.save(todos.value);
     };
     const onUpdateItem = (value: { id: number; title: string }) => {
         const index = todos.value.findIndex(v => v.id === value.id);
         todos.value[index].title = value.title;
+        storage.save(todos.value);
     };
+
 </script>
 
 <style lang="scss">
