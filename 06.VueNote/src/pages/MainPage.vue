fcaaa36da0a94ae77dd90188f1b326f6a18444cc
GalaDriel <g_ala@naver.com>
2023-04-18 14:23:59 +0900

    Add: Component - NoteItem

--- 06.VueNote/src/pages/MainPage.vue
+++ 06.VueNote/src/pages/MainPage.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <ul v-if="notes.length > 0">
+            <NoteItem v-for="note in notes" :item="note" :key="note.id" />
+        </ul>
         <div class="btnWrap">
             <button class="xi-plus" @click="onClickCreate"></button>
         </div>
@@ -8,11 +11,18 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { useItem } from '@/store/itemStore';
+import NoteItem from '@/components/note/NoteItem.vue';
+import { storeToRefs } from 'pinia';
 
+const item = useItem();
+const { notes } = storeToRefs(item);
+const { GET_NOTE } = item;
 const router = useRouter();
 const onClickCreate = () => {
     router.push('/create');
 };
+GET_NOTE();
 </script>
 
 <style lang="scss" scoped>
