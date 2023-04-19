580700c07b2217197904c63c513d9f302ded6a0a
GalaDriel <g_ala@naver.com>
2023-04-19 16:58:18 +0900

    Update: Define Components - TodoHeader

--- 07.VueFB/src/App.vue
+++ 07.VueFB/src/App.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>App</div>
+    <div class="container">
+        <TodoHeader />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TodoHeader from './components/TodoHeader.vue';
+</script>
 
 <style lang="scss">
 @import '@/assets/scss/index.scss';
---
+++ 07.VueFB/src/components/TodoHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <header>
+        <h1>Vue FireBase</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/TodoHeader';
+</style>
