bcdfd9ea1c532004f6eb9da848ba4b678eb2845c
GalaDriel <g_ala@naver.com>
2023-04-19 17:37:48 +0900

    Update: Connect firebase to vue

--- 07.VueFB/src/App.vue
+++ 07.VueFB/src/App.vue
@@ -8,6 +8,8 @@
 <script setup lang="ts">
 import TodoHeader from './components/TodoHeader.vue';
 import TodoLoading from './components/TodoLoading.vue';
+import { app } from '@/utils/firebase';
+console.log(app);
 </script>
 
 <style lang="scss">
